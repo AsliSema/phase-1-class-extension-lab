ad095eb173a89bfed345ad06fb3db4aacd33528f
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-04-06 10:55:07 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon {
+    constructor(arr){
+        this.arr = arr
+    }
+    get countSides(){
+        return this.arr.length
+    }
+    get perimeter(){
+        let sumOfSides = 0;
+        this.arr.forEach(side => {
+            sumOfSides += side
+        });
+        return sumOfSides
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        let sortedArr = this.arr.sort((a,b)=> a-b)
+        let maxVal = sortedArr[2];
+        if(sortedArr[1]+sortedArr[0] > maxVal){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        let initialVal = this.arr[0]
+        let isSquare;
+        this.arr.forEach(item => {
+            if(item === initialVal){
+                isSquare = true
+            }else{
+                isSquare = false
+            }
+        })
+        return isSquare
+    }
+    get area(){
+        return this.arr[0]*this.arr[0]
+    }
+}
